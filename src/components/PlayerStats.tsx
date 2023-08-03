f65d13bca1cd3f222054f97f1424bae58bb9566b
zachmanac <zachstephenson94@gmail.com>
2023-08-02 17:25:04 -0700

    add conditional render for no player searched

--- src/components/PlayerStats.tsx
+++ src/components/PlayerStats.tsx
@@ -17,31 +17,37 @@ const PlayerStats = ({ playerStats }: PlayerStatsProps) => {
   console.log('playerStats: in playerstats component', playerStats);
 
   return (
-    <div className='bg-gray-100 w-[90%] mx-auto text-center min-w-full'>
-      <div className='grid grid-cols-7 font-bold'>
-        <p className='text-left'>Name</p>
-        <p>GP</p>
-        <p>Goals</p>
-        <p>Assists</p>
-        <p>Points</p>
-        <p>+/-</p>
-        <p>PIM</p>
+    <div>
+      {playerStats.length === 0 ? (
+        <div></div>
+      ) : (
+        <div className='bg-gray-100 w-[90%] mx-auto text-center min-w-full'>
+        <div className='grid grid-cols-7 font-bold'>
+          <p className='text-left'>Name</p>
+          <p>GP</p>
+          <p>Goals</p>
+          <p>Assists</p>
+          <p>Points</p>
+          <p>+/-</p>
+          <p>PIM</p>
+        </div>
+        <div>
+          {playerStats.map((player: any) => (
+            <div key={player.playerId} className='grid grid-cols-7'>
+              <p className='text-left'>{player.skaterFullName}</p>
+              <p>{player.gamesPlayed}</p>
+              <p>{player.goals}</p>
+              <p>{player.assists}</p>
+              <p>{player.points}</p>
+              <p>{player.plusMinus}</p>
+              <p>{player.penaltyMinutes}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        {playerStats.map((player: any) => (
-          <div key={player.playerId} className='grid grid-cols-7'>
-            <p className='text-left'>{player.skaterFullName}</p>
-            <p>{player.gamesPlayed}</p>
-            <p>{player.goals}</p>
-            <p>{player.assists}</p>
-            <p>{player.points}</p>
-            <p>{player.plusMinus}</p>
-            <p>{player.penaltyMinutes}</p>
-          </div>
-        ))}
-      </div>
+      )}
     </div>
-  )
+  );
 };
 
 export default PlayerStats;
